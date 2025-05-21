7ceed9ae8b64bb06d56ade038203e9cf5b4d5380
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-05-21 11:32:21 -0300

    Create DemoEvento.java

---
+++ Unidade1/classes/eventos/DemoEvento.java
@@ -0,0 +1,16 @@
+package Java.classes.eventos;
+
+public class DemoEvento {
+    public static void main(String[] args) {
+    
+    EventoAcademico ev= new EventoAcademico("GoRn", "UFRN", 500);
+        
+        ev.mostraEvento();
+    }
+}
+
+//construtor padrao 
+//inicializa com null e os valores numeros com 0
+
+//ao criar o proprio construtor 
+//o construtor padrao deixa de existir
