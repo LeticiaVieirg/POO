e252d0b43c8f819e4864452785f4974f929112cd
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-05-21 11:39:25 -0300

    Create DemoCirculo.java

---
+++ Unidade1/classes/circulo/DemoCirculo.java
@@ -0,0 +1,12 @@
+package Java.classes.circulo;
+
+public class DemoCirculo {
+
+    public static void main(String[] args) {
+        Circulo c1 = new Circulo (1);
+        Circulo c2 = new Circulo (2);
+        System.out.println(c1.perimetroCirculo());
+        System.out.println(c2.perimetroCirculo());
+
+    }
+}
