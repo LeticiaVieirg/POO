bec77b3631de0de9cd4b4cc8a5706d746644aa79
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-05-26 14:20:06 -0300

    Create coordenadas.java

---
+++ Unidade1/introi/coordenadas.java
@@ -0,0 +1,26 @@
+package unidade1;
+
+import java.util.Scanner;
+
+public class q11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite as coordenadas do primeiro ponto (x y):");
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        
+        System.out.println("Digite as coordenadas do segundo ponto (x y):");
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+        
+        String posicao = "";
+        if (y2 > y1) posicao += "acima ";
+        else if (y2 < y1) posicao += "abaixo ";
+        
+        if (x2 > x1) posicao += "à direita";
+        else if (x2 < x1) posicao += "à esquerda";
+        
+        System.out.println("O segundo ponto está " + (posicao.isEmpty() ? 
+        "na mesma posição" : posicao));
+    }
+}
