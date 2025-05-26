f5cdd1ef12f899e0c1f5de1268532a46ce8161d1
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-05-26 14:17:24 -0300

    Create angulo.java

---
+++ Unidade1/introi/angulo.java
@@ -0,0 +1,35 @@
+
+package unidade1;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+
+        Scanner ler=new Scanner(System.in);
+
+        double graus=ler.nextDouble();
+        double rad=Math.toRadians(graus);
+
+        double seno=Math.sin(rad);
+        double cos=Math.cos(rad);
+        double tangente=Math.tan(rad);
+
+        System.out.println("Digite o valor do grau: ");
+        graus=ler.nextDouble();
+
+        System.out.println("Seno: "+seno);
+        System.out.println("Cosseno: "+cos);
+        System.out.println("Tangente: "+tangente);
+
+        double cossecante=1/seno;
+        double secante=1/cos;
+        double cotangente=1/tangente;
+
+        System.out.println("Cossecante: " + cossecante);
+        System.out.println("Secante: "+ secante);
+        System.out.println("Cotangente: "+ cotangente);
+
+        ler.close();
+    }
+}
