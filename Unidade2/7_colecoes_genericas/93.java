c02e8e70c6e2dc5f84071389fdb219ba374825e8
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-06-21 17:20:04 -0300

    Create 93.java

---
+++ Unidade2/7_colecoes_genericas/93.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class InverterLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Character> lista = new LinkedList<>();
+
+        // Adicionando 10 caracteres
+        for (char c = 'A'; c <= 'J'; c++) {
+            lista.add(c);
+        }
+
+        System.out.println("Lista original: " + lista);
+
+        // Criar a lista invertida manualmente
+        LinkedList<Character> invertida = new LinkedList<>();
+        for (int i = lista.size() - 1; i >= 0; i--) {
+            invertida.add(lista.get(i));
+        }
+
+        System.out.println("Lista invertida: " + invertida);
+    }
+}
