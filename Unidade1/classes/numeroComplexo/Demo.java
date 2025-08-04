800b184e7c2bfb87f98d3f22d5e0c3b9832826b4
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-08-04 15:11:21 -0300

    Create Demo.java

---
+++ Unidade1/classes/numeroComplexo/Demo.java
@@ -0,0 +1,6 @@
+public class DemoNumeroComplexo {
+    public static void main(String[] args) {
+        NumeroComplexo c1 = new NumeroComplexo(2, 3);
+        System.out.println("Número: " + c1);
+    }
+}
